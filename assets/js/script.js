0fe2354245f5e14b31333a9bdb1d71309fa809ef
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-21 23:40:31 +0100

    add section and function
    add section error age and function button in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,79 @@ document.getElementById('calcola').addEventListener('click', function(){
 
     const userAge = document.getElementById('userage').value;
     console.log(userAge);
+
+    let ticketPrice  = userKm * 0.21;
+    console.log(ticketPrice.toFixed(2));
+
+    let ticketDiscount = 0;
+
+    if (userAge == "minorenne"){
+
+        ticketDiscount = ticketPrice * 0.20 ;
+        let ticketFullPrice = ticketPrice - ticketDiscount;
+        console.log(ticketFullPrice.toFixed(2));
+
+        document.getElementById("username").innerHTML = userFullName;
+        document.getElementById("discount").innerHTML = "Biglietto Under 18";
+
+        let carrozza = Math.round(Math.random()*10)+1;
+        console.log(carrozza);
+        document.getElementById("carrozza").innerHTML = carrozza;
+
+        let codiceCp = Math.round(Math.random()*10000)+1;
+        console.log(codiceCp);
+        document.getElementById("codicecp").innerHTML = codiceCp;
+
+        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
+
+        document.getElementById("sectionTicket").classList.remove('d-none');
+    } else if (userAge == "over"){
+
+        ticketDiscount = ticketPrice * 0.40 ;
+        let ticketFullPrice = ticketPrice - ticketDiscount;
+        console.log(ticketFullPrice.toFixed(2));
+
+        document.getElementById("username").innerHTML = userFullName;
+        document.getElementById("discount").innerHTML = "Biglietto Over 65";
+
+        let carrozza = Math.round(Math.random()*10)+1;
+        console.log(carrozza);
+        document.getElementById("carrozza").innerHTML = carrozza;
+
+        let codiceCp = Math.round(Math.random()*10000)+1;
+        console.log(codiceCp);
+        document.getElementById("codicecp").innerHTML = codiceCp;
+
+        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
+
+        document.getElementById("sectionTicket").classList.remove('d-none');
+
+    } else if ( userAge == "maggiorenne"){
+
+        let ticketFullPrice = ticketPrice - ticketDiscount;
+        console.log(ticketFullPrice.toFixed(2));
+
+        document.getElementById("username").innerHTML = userFullName;
+        document.getElementById("discount").innerHTML = "Biglietto Standard";
+
+        let carrozza = Math.round(Math.random()*10)+1;
+        console.log(carrozza);
+        document.getElementById("carrozza").innerHTML = carrozza;
+
+        let codiceCp = Math.round(Math.random()*10000)+1;
+        console.log(codiceCp);
+        document.getElementById("codicecp").innerHTML = codiceCp;
+
+        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
+
+        document.getElementById("sectionTicket").classList.remove('d-none');
+
+    } else {
+        document.getElementById("sectionDates").classList.add('d-none');
+        document.getElementById("sectionErrorAge").classList.remove('d-none');
+        document.getElementById("sectionErrorAge").classList.add('d-flex');
+    }
 })
-
-
-
-//document.querySelector(".d-none").classList.remove('d-none');
-
-
-
-
-
-
-
-
-
-
 
 
 document.getElementById('annulla').addEventListener('click', function(){
@@ -32,4 +90,12 @@ document.getElementById('annulla').addEventListener('click', function(){
     document.getElementById('userkm').value = "";
 
     document.getElementById('userage').value = "";
+})
+
+
+document.getElementById('esci').addEventListener('click', function(){
+
+    document.getElementById("sectionDates").classList.remove('d-none');
+    document.getElementById("sectionErrorAge").classList.add('d-none');
+    document.getElementById("sectionErrorAge").classList.remove('d-flex');
 })
