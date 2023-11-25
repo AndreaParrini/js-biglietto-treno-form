a5f373cdc848fb1b2da14a23296325d1e8dddfda
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-25 10:04:53 +0100

    fix file js and tag input in html
    remove all row duplicate and clean code in js
    modify attribute type to tag input km (text<number)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,70 @@
-document.getElementById('calcola').addEventListener('click', function(){
+let userFullName = "";
+let userKm = "";
+let userAge = "";
+let ticketDiscount = 0;
+let ticketPrice = 0;
+let ticketFullPrice = 0;
 
-    const userFullName = document.getElementById('userfullname').value;
+document.getElementById('calcola').addEventListener('click', function () {
+
+    userFullName = document.getElementById('userfullname').value;
     console.log(userFullName);
 
-    const userKm = parseInt(document.getElementById('userkm').value);
+    userKm = parseInt(document.getElementById('userkm').value);
     console.log(userKm);
 
-    const userAge = document.getElementById('userage').value;
+    userAge = document.getElementById('userage').value;
     console.log(userAge);
 
-    let ticketPrice  = userKm * 0.21;
+    ticketPrice = userKm * 0.21;
     console.log(ticketPrice.toFixed(2));
 
-    let ticketDiscount = 0;
+    if (userAge == "minorenne") {
 
-    if (userAge == "minorenne"){
+        ticketDiscount = ticketPrice * 0.20;
 
-        ticketDiscount = ticketPrice * 0.20 ;
-        let ticketFullPrice = ticketPrice - ticketDiscount;
-        console.log(ticketFullPrice.toFixed(2));
-
-        document.getElementById("username").innerHTML = userFullName;
         document.getElementById("discount").innerHTML = "Biglietto Under 18";
 
-        let carrozza = Math.round(Math.random()*10)+1;
-        console.log(carrozza);
-        document.getElementById("carrozza").innerHTML = carrozza;
+    } else if (userAge == "over") {
 
-        let codiceCp = Math.round(Math.random()*10000)+1;
-        console.log(codiceCp);
-        document.getElementById("codicecp").innerHTML = codiceCp;
+        ticketDiscount = ticketPrice * 0.40;
 
-        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
-
-        document.getElementById("sectionTicket").classList.remove('d-none');
-    } else if (userAge == "over"){
-
-        ticketDiscount = ticketPrice * 0.40 ;
-        let ticketFullPrice = ticketPrice - ticketDiscount;
-        console.log(ticketFullPrice.toFixed(2));
-
-        document.getElementById("username").innerHTML = userFullName;
         document.getElementById("discount").innerHTML = "Biglietto Over 65";
 
-        let carrozza = Math.round(Math.random()*10)+1;
-        console.log(carrozza);
-        document.getElementById("carrozza").innerHTML = carrozza;
+    } else if (userAge == "maggiorenne") {
 
-        let codiceCp = Math.round(Math.random()*10000)+1;
-        console.log(codiceCp);
-        document.getElementById("codicecp").innerHTML = codiceCp;
+        ticketFullPrice = ticketPrice - ticketDiscount;
 
-        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
-
-        document.getElementById("sectionTicket").classList.remove('d-none');
-
-    } else if ( userAge == "maggiorenne"){
-
-        let ticketFullPrice = ticketPrice - ticketDiscount;
-        console.log(ticketFullPrice.toFixed(2));
-
-        document.getElementById("username").innerHTML = userFullName;
         document.getElementById("discount").innerHTML = "Biglietto Standard";
-
-        let carrozza = Math.round(Math.random()*10)+1;
-        console.log(carrozza);
-        document.getElementById("carrozza").innerHTML = carrozza;
-
-        let codiceCp = Math.round(Math.random()*10000)+1;
-        console.log(codiceCp);
-        document.getElementById("codicecp").innerHTML = codiceCp;
-
-        document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
-
-        document.getElementById("sectionTicket").classList.remove('d-none');
 
     } else {
         document.getElementById("sectionDates").classList.add('d-none');
         document.getElementById("sectionErrorAge").classList.remove('d-none');
         document.getElementById("sectionErrorAge").classList.add('d-flex');
     }
+
+    ticketFullPrice = ticketPrice - ticketDiscount;
+    console.log(ticketFullPrice.toFixed(2));
+
+    document.getElementById("username").innerHTML = userFullName;
+
+    let carrozza = Math.round(Math.random() * 10) + 1;
+    console.log(carrozza);
+    document.getElementById("carrozza").innerHTML = carrozza;
+
+    let codiceCp = Math.round(Math.random() * 10000) + 1;
+    console.log(codiceCp);
+    document.getElementById("codicecp").innerHTML = codiceCp;
+
+    document.getElementById("ticketprice").innerHTML = `${ticketFullPrice.toFixed(2)}€`;
+
+    document.getElementById("sectionTicket").classList.remove('d-none');
+
 })
 
 
-document.getElementById('annulla').addEventListener('click', function(){
+
+document.getElementById('annulla').addEventListener('click', function () {
 
     document.getElementById('userfullname').value = "";
 
@@ -95,9 +76,12 @@ document.getElementById('annulla').addEventListener('click', function(){
 })
 
 
-document.getElementById('esci').addEventListener('click', function(){
+document.getElementById('esci').addEventListener('click', function () {
 
     document.getElementById("sectionDates").classList.remove('d-none');
+
     document.getElementById("sectionErrorAge").classList.add('d-none');
+
     document.getElementById("sectionErrorAge").classList.remove('d-flex');
+
 })
